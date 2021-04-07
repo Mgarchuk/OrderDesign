a5be10f8b094a5f3054cfa0b73c1a5326386da7e
Marina <mgarchuk6@gmail.com>
2021-04-07 18:50:09 +0300

    Added logs for order service

--- src/main/java/by/student/orderDesign/service/OrderService.java
+++ src/main/java/by/student/orderDesign/service/OrderService.java
@@ -1,15 +1,20 @@
 package by.student.orderDesign.service;
 
+import by.student.orderDesign.domain.Address;
 import by.student.orderDesign.domain.Order;
 import by.student.orderDesign.domain.OrderStatus;
 import by.student.orderDesign.storage.OrderStorage;
 import com.google.common.base.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class OrderService {
 
     private final OrderStorage storage;
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderService.class.getName());
 
     public OrderService(OrderStorage orderStorage) {
         this.storage = orderStorage;
@@ -25,13 +30,20 @@ public class OrderService {
         Preconditions.checkNotNull(order.getExpectedDeliveryDate(), "Null date");
         Preconditions.checkNotNull(order.getStatus(), "Null status");
 
+        logger.info("Order with id: {} was successfully checked for null fields", order.getId());
+
         order.setStatus(OrderStatus.ACCEPTED);
 
         storage.addOrder(order);
+
+        logger.info("Order with orderId: {} placed", order.getId());
     }
 
     public List<Order> loadAllByUserId(String userId) {
         Preconditions.checkNotNull(userId, "Null user id");
-        return storage.getOrdersByUserId(userId);
+        List<Order> userOrders = storage.getOrdersByUserId(userId);
+
+        logger.info("Orders for user with id: {} was loaded. Orders count: {}", userId, userOrders.size());
+        return userOrders;
     }
 }
